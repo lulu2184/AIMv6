88411974996a62004315614d7d59ae34de6dc31b
lulu2184 <lulu2184@gmail.com>
2015-11-04 09:19:41 +0800

    finish mbr

--- boot/arch/armv7a-le/zynq7000/bootmain.c
+++ boot/arch/armv7a-le/zynq7000/bootmain.c
@@ -1,24 +1,38 @@
-/*
- * Copyright (C) 2015 David Gao <davidgao1001@gmail.com>
- *
- * This program is free software; you can redistribute  it and/or modify it
- * under  the terms of  the GNU General  Public License as published by the
- * Free Software Foundation;  either version 2 of the  License, or (at your
- * option) any later version.
- *
- */
+#define PRELOAD_VECTOR_BASE 0x1ff00000
 
-#include <config.h>
-
-#define PRELOAD_VECTOR_BASE	0x1ff00000
-
-void (*uart_init)(void) = (void *)(PRELOAD_VECTOR_BASE + 0x4);
-void (*uart_enable)(void) = (void *)(PRELOAD_VECTOR_BASE + 0x8);
+#include "sys/types.h"
 void (*uart_spin_puts)(const char *) = (void *)(PRELOAD_VECTOR_BASE + 0xC);
+int (*sd_dma_spin_read)(u32, u16, u32) = (void *)0x1ff00010;
+void (*puthex)(u32) = (void *)0x1ff00014;
 
-void mbr_bootmain(void)
-{
-	uart_spin_puts("Hello, AIMv6!\r\n");
-	while(1);
+void mbr_bootmain(void) {
+    uart_spin_puts("Jump into MBR\r\n");
+
+	volatile u8 *mbr = (void *)0x200002;
+    sd_dma_spin_read((u32)mbr, 1, 0);
+    u32 partition2_start = *(u32*)(mbr + 0x1D6);
+
+	/* read header of ELF from SD card to memory (address: header_start) */
+	volatile u8 *header_start = (void *)0x300000;
+	sd_dma_spin_read((u32)header_start, 3, partition2_start);
+
+	void (*entry_point)(void) = (void *)(*(u32*)(header_start + 0x18));
+	u16 program_header_size = *(u16*)(header_start + 0x2A);
+	u16 program_header_count = *(u16*)(header_start + 0x2C);
+
+	u32 program_header = *(u32*)(header_start + 0x1C);
+    program_header += (u32)header_start;
+	for (int i = 0; i < program_header_count; i++) {
+		u32 p_offset = *(u32*)(program_header + 4);
+		u32 p_paddr = *(u32*)(program_header + 12);
+		u32 p_filesz = *(u32*)(program_header + 16);
+		u16 p_count = (u16) (((p_paddr + p_filesz) >> 9) - (p_paddr >> 9) + 1);
+		p_offset = p_offset >> 9;
+		p_paddr = p_paddr >> 9 << 9; 
+
+		sd_dma_spin_read((u32)p_paddr, p_count, partition2_start + p_offset);
+		program_header = program_header + (program_header_size >> 2);
+	}
+
+    entry_point();
 }
-
