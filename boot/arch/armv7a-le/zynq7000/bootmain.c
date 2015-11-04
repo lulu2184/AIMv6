22041503c64a9bc84a1f416b2c3bf34a3221cec2
lulu2184 <lulu2184@gmail.com>
2015-11-04 09:33:51 +0800

    program cleanup

--- boot/arch/armv7a-le/zynq7000/bootmain.c
+++ boot/arch/armv7a-le/zynq7000/bootmain.c
@@ -6,11 +6,11 @@ int (*sd_dma_spin_read)(u32, u16, u32) = (void *)0x1ff00010;
 void (*puthex)(u32) = (void *)0x1ff00014;
 
 void mbr_bootmain(void) {
-    uart_spin_puts("Jump into MBR\r\n");
+	uart_spin_puts("Jump into MBR\r\n");
 
 	volatile u8 *mbr = (void *)0x200002;
-    sd_dma_spin_read((u32)mbr, 1, 0);
-    u32 partition2_start = *(u32*)(mbr + 0x1D6);
+	sd_dma_spin_read((u32)mbr, 1, 0);
+	u32 partition2_start = *(u32*)(mbr + 0x1D6);
 
 	/* read header of ELF from SD card to memory (address: header_start) */
 	volatile u8 *header_start = (void *)0x300000;
@@ -21,7 +21,7 @@ void mbr_bootmain(void) {
 	u16 program_header_count = *(u16*)(header_start + 0x2C);
 
 	u32 program_header = *(u32*)(header_start + 0x1C);
-    program_header += (u32)header_start;
+	program_header += (u32)header_start;
 	for (int i = 0; i < program_header_count; i++) {
 		u32 p_offset = *(u32*)(program_header + 4);
 		u32 p_paddr = *(u32*)(program_header + 12);
@@ -34,5 +34,5 @@ void mbr_bootmain(void) {
 		program_header = program_header + (program_header_size >> 2);
 	}
 
-    entry_point();
+	entry_point();
 }
--- hello/hello.c
+++ hello/hello.c
@@ -1,7 +1,7 @@
 void hello(void) {
-    void (*uart_spin_puts)(const char *) = (void *)0x1FF0000C;
+	void (*uart_spin_puts)(const char *) = (void *)0x1FF0000C;
 
-    uart_spin_puts("Hello 2nd partition!\r\n");
-    while (1);
-    return;
+	uart_spin_puts("Hello 2nd partition!\r\n");
+	while (1);
+	return;
 }
