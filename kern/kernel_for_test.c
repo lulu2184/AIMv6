de1f69f8997fa6363b9ca0dff1176335731b6c49
lulu2184 <lulu2184@gmail.com>
2015-12-05 16:40:33 +0800

    fix bug in test

--- kern/kernel_for_test.c
+++ kern/kernel_for_test.c
@@ -11,25 +11,21 @@ void test_fail() {
 void pages_manager_test() {
 	uart_spin_puts("PAGES MANAGER TEST: ");
 	char* addr = alloc_pages(2);
-	puthex((unsigned)addr);
-	// if ((unsigned)addr != 0x9EFE0000) test_fail();
+	if ((unsigned)addr != 0x9EFE0000) test_fail();
 	free_pages(addr, 1);
 	unsigned first_size = get_kmem_first_size();
-	puthex(first_size);
-	// if (first_size != 0x1EDF) test_fail();
+	if (first_size != 0x1E0F) test_fail();
 	addr = alloc_pages(2);
-	// if (first_size != 0x1EDD) test_fail();
-	puthex(first_size);
-	// if ((unsigned)addr != 0x9EFD0000) test_fail();
-	puthex((unsigned)addr);
+	first_size = get_kmem_first_size();
+	if (first_size != 0x1E0D) test_fail();
+	if ((unsigned)addr != 0x9EFD0000) test_fail();
 	free_pages(addr + PAGE_SIZE, 1);
 	first_size = get_kmem_first_size();
-	puthex(first_size);
-	// if (first_size != 0x1EDD) test_fail();
+	if (first_size != 0x1E0D) test_fail();
 	free_pages(addr, 1);
 	first_size = get_kmem_first_size();
-	puthex(first_size);
-	// if (first_size != 0x1EDF) test_fail();
+	if (first_size != 0x1E0F) test_fail();
+	uart_spin_puts("PAGES MANAGER TEST: ");
 	uart_spin_puts("PASS\r\n");
 }
 
@@ -38,6 +34,9 @@ int kernel_entry() {
 	uart_spin_puts("Hello kernel!\r\n");
 	init_first_page_table();
 	uart_spin_puts("Kernel space!: PC = ");
+	asm volatile("mov %0 ,pc" : "=r"(tmp_pc));
+	puthex(tmp_pc);
+	// TODO!! if delete the two lines below, bug appears
 	asm volatile("mov %0 ,pc" : "=r"(tmp_pc));
 	puthex(tmp_pc);
 	alloc_init();
