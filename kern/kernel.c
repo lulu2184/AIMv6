046ca29cdd04da3ebcc82546820f08c4fc9c8c89
lulu2184 <lulu2184@gmail.com>
2015-12-06 14:04:49 +0800

    update kernel

--- kern/kernel.c
+++ kern/kernel.c
@@ -9,7 +9,20 @@
 int kernel_entry() {
 	uart_spin_puts("Hello kernel!\r\n");
 	init_first_page_table();
+	unsigned tmp;
+	asm volatile(
+		"ldr fp, =0x80F00000\r\n"
+		"mov sp, fp"
+	);
+	remove_low_mapping();
+	uart_spin_puts("Kernel space!: PC = ");
+	asm volatile("mov %0 ,pc" : "=r"(tmp));
+	puthex(tmp);
 	alloc_init();
+	uart_spin_puts("SP = ");
+	asm volatile("mov %0 ,sp" : "=r"(tmp));
+	puthex(tmp);
+
 
 	while (1);
 }
